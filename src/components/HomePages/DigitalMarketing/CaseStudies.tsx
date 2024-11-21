e680d3696bb85f214149b831406607a29f268f47
akankshacodess <guptaakanksha2002@gmail.com>
2024-11-21 18:56:38 +0530

    carousel

--- src/components/HomePages/DigitalMarketing/CaseStudies.tsx
+++ src/components/HomePages/DigitalMarketing/CaseStudies.tsx
@@ -7,7 +7,8 @@ import Image from "next/image";
 const caseStudiesData = [
   {
     image: "/images/case-studies/studie1.jpg",
-    title: "How a global retailer achieved a 40% productivity boost using Microsoft Power Automate.",
+    title:
+      "How a global retailer achieved a 40% productivity boost using Microsoft Power Automate.",
     category: "Web, Sales",
     detailsLink: "/portfolio/ai-email-reducing-time/",
     shapeImage: "/images/case-studies/studie-shape1.png",
@@ -15,7 +16,26 @@ const caseStudiesData = [
   },
   {
     image: "/images/case-studies/studie2.jpg",
-    title: "Simplifying project management for a construction firm with custom Power BI dashboards.",
+    title:
+      "Simplifying project management for a construction firm with custom Power BI dashboards.",
+    category: "Sales, SEO, Web",
+    detailsLink: "/portfolio/automating-sales-order-using-ai-email/",
+    shapeImage: "/images/case-studies/studie-shape2.png",
+    aosDelay: "200",
+  },
+  {
+    image: "/images/case-studies/studie1.jpg",
+    title:
+      "How a global retailer achieved a 40% productivity boost using Microsoft Power Automate.",
+    category: "Web, Sales",
+    detailsLink: "/portfolio/ai-email-reducing-time/",
+    shapeImage: "/images/case-studies/studie-shape1.png",
+    aosDelay: "100",
+  },
+  {
+    image: "/images/case-studies/studie2.jpg",
+    title:
+      "Simplifying project management for a construction firm with custom Power BI dashboards.",
     category: "Sales, SEO, Web",
     detailsLink: "/portfolio/automating-sales-order-using-ai-email/",
     shapeImage: "/images/case-studies/studie-shape2.png",
@@ -39,13 +59,14 @@ const CaseStudies: React.FC = () => {
           <div className="section-title">
             <h2>CASE STUDIES</h2>
             <p>
-            See how our AI solutions have transformed businesses, reducing response times and boosting conversions.
+              See how our AI solutions have transformed businesses, reducing
+              response times and boosting conversions.
             </p>
           </div>
 
           <div className="row justify-content-center">
             {caseStudiesData &&
-              caseStudiesData.slice(0, 3).map((value, i) => (
+              caseStudiesData.slice(0, 4).map((value, i) => (
                 <div className="col-lg-4 col-md-6" key={i}>
                   <div
                     className="single-case-studies"
@@ -82,8 +103,11 @@ const CaseStudies: React.FC = () => {
           </div>
 
           <div className="view-more-work">
-            <Link href="https://calendly.com/dhruv-aii/30mins" className="btn btn-primary">
-            Book a 1:1 Call
+            <Link
+              href="https://calendly.com/dhruv-aii/30mins"
+              className="btn btn-primary"
+            >
+              Book a 1:1 Call
             </Link>
           </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
